858f9c07459c9aa99068be2495bd634dbe7b1851
Siddesha KR <siddeshkr03@example.com>
2026-03-31 18:17:26 +0530

    intro to JavaScript

---
+++ JavaScript/first-js.js
@@ -0,0 +1,14 @@
+console.log('first code');
+
+var name = "Rahul"
+
+console.log(name);
+
+let city = "Bengaluru"
+console.log('I reside in',city);
+
+const state  = "Karnataka"
+console.log('My state is ',state);
+
+
+
---
+++ JavaScript/learn-variables.js
@@ -0,0 +1,20 @@
+var name = "VK"
+var name = "DK"
+console.log('my name is ',name);
+
+let city = "Bengaluru"
+city = 'Tumakuru'
+console.log('I reside in',city);
+
+const state = "Karnataka"
+console.log('state',state);
+
+// scope of variables
+
+
+{
+    const score = 79
+    console.log('Rohith score', score);
+    
+}
+console.log('Rohith score--', score);git in
