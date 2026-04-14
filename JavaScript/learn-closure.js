d0b9eb72f1a0a10af680096b832b5c3f627626e7
Siddesha KR <siddeshkr03@example.com>
2026-04-14 11:02:34 +0530

    JS events

---
+++ JavaScript/learn-closure.js
@@ -0,0 +1,33 @@
+// Closure is the process or method of retaining the outer variable value even after outer value is closed
+
+// Example 1
+function outer() {
+  let value = 0; // outer variable
+
+  function inner() {
+    value++;
+    console.log("My value", value);
+  }
+  return inner;
+}
+
+let out = outer();
+out();
+out();
+out();
+
+// Example 2
+
+function teashop() {
+  let bill = 0; // outer variable
+
+  return function addBill(price) {
+    bill += price;
+    console.log("Total bill: ₹" + bill);
+  };
+}
+
+const myBill = teashop();
+myBill(10); // Tea
+myBill(20); // Snacks
+myBill(15); // coffee
--- JavaScript/scopes.js
+++ JavaScript/scopes.js
@@ -1,32 +1,32 @@
-// // Learning concept : Lexical Scoping
+//  Learning concept : Lexical Scoping
 
-// // Global scope
-// // Local scope
-// // Nested scope
-// // block scope
+//  Global scope
+//  Local scope
+//  Nested scope
+//  block scope
 
-// let ad = "Five star Ad"; // global scope
+let ad = "Five star Ad"; // global scope
 
-// function ramesh(){
-//     let one = "Ramesh is on the set ";// local scope
-//     console.log(ad);
-//     console.log(one);
+function ramesh(){
+    let one = "Ramesh is on the set ";// local scope
+    console.log(ad);
+    console.log(one);
 
-//     function suresh(){
-//     let two = "Suresh on the set"; // nested scope
+    function suresh(){
+    let two = "Suresh on the set"; // nested scope
 
-//     console.log(ad);
-//     console.log(one);
-//     console.log(two);
+    console.log(ad);
+    console.log(one);
+    console.log(two);
 
-//     if(true){
-//         let final = "Ad is done"; // block scope
-//         console.log(final);
-//     }
-// }
-//     suresh()
-// }
-// ramesh()
+    if(true){
+        let final = "Ad is done"; // block scope
+        console.log(final);
+    }
+}
+    suresh()
+}
+ramesh()
 
 let rs = "Play aggressive"; // global
 
