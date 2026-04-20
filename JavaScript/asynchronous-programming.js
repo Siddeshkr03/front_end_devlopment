ff460c02a18667092a507acf920f6d012d4d3b6a
Siddesha KR <siddeshkr03@gmail.com>
2026-04-20 10:12:34 +0530

    setTimeout

---
+++ JavaScript/asynchronous-programming.js
@@ -0,0 +1,11 @@
+
+function one(){
+    console.log("Function one");   
+}
+
+function two(){
+    console.log("Function two");
+}
+
+one();
+two();
---
+++ JavaScript/learn-timeout-interval.js
@@ -0,0 +1,7 @@
+setTimeout(()=>{
+    console.log("I am printing the waited information");
+}, 3000);
+
+setInterval(()=>{
+    console.log("I am printing the waited information");
+}, 1000);
