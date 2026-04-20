2971a1c4c90b77377519ce45fa69e6df0f08fbcb
Siddesha KR <siddeshkr03@gmail.com>
2026-04-20 10:19:07 +0530

    Asynchronous functions

--- JavaScript/asynchronous-programming.js
+++ JavaScript/asynchronous-programming.js
@@ -1,10 +1,14 @@
 
 function one(){
-    console.log("Function one");   
+    setTimeout(() => {
+        console.log("Function one");   
+    }, 3000);
 }
 
 function two(){
-    console.log("Function two");
+    setTimeout(() => {
+        console.log("Function two");   
+    }, 1000);
 }
 
 one();
