b7a3e9a143c2ada0772a9004bfbe901e2d3b328d
Siddesha KR <siddeshkr03@gmail.com>
2026-04-28 18:01:25 +0530

    Movies example

--- react/src/components/apr-28/Movies.jsx
+++ react/src/components/apr-28/Movies.jsx
@@ -1,11 +1,29 @@
+import "./Movies.css";
+
 const Movies = ({ MoviesData }) => {
   return (
     <div>
-      {MoviesData.map((movie, index) => (
-        <div >
-          <img src={movie.imageUrl} height={600}/>
-        </div>
-      ))}
+      <div className="cards">
+        {MoviesData.map((movie, index) => (
+          <div key={index} style={{ position: "relative" }}>
+            <img src={movie.imageUrl} width={300} height={600} />
+
+            {index === 2 && (
+              <img
+                src="https://brandlogovector.com/wp-content/uploads/2022/02/Disney-Hotstar-Logo-Small.png"
+                alt="Disney+ Hotstar"
+                style={{
+                  position: "absolute",
+                  top: "190px",
+                  left: "100px",
+                  width: "150px",
+                  zIndex: 10,
+                }}
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
